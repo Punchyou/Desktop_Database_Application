68471b0afafd01bd6d706a94a8f35c93d8e91d96
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-02-16 13:53:11 +0000

    Refactor with oop style part 1

--- backend.py
+++ backend.py
@@ -10,50 +10,33 @@ import sqlite3
 class Database:
     
     def __init__(self, db):
-        conn = sqlite3.connect(db)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year interger, isbn integer)")
-        conn.commit()
-        conn.close()
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year interger, isbn integer)")
+        self.conn.commit()
 
     #insert the data
     def insert(self, title, author, year, isbn):
-        conn = sqlite3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
-        conn.commit()
-        conn.close()
+        self.cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+        self.conn.commit()
     
     #fetch the data
     def view(self):
-        conn = sqlite3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM book")
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
         return rows
     
     #search func
     def search(self, title = "", author = "", year = "", isbn = ""):
-        conn = sqlite3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("SELECT *  FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT *  FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
+        rows = self.cur.fetchall()
         return rows
     
     #delete a record
     def delete(self, id):
-        conn = sqlite3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("DELETE FROM book WHERE id = ?", (id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM book WHERE id = ?", (id,))
     
     #update a record
     def update(self, id, title, author, year, isbn):
-        conn = sqlite3.connect("books.db")
-        cur = conn.cursor()
-        cur.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
+        
